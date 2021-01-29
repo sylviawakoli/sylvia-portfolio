aaaaac66370aa27a69b1dcd41fde1900db8a0dd0
sylviawakoli <swakoli47@gmail.com>
2021-01-29 10:14:47 +0300

    Added some some pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
 import { HomeComponent } from './home/home.component';
+import { MyWorkComponent } from './my-work/my-work.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "my-work",
+    component: MyWorkComponent
+  },
+  {
+    path: "contacts",
+    component: ContactsComponent
   },
   {
     path: "**",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    MyWorkComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
